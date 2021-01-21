88970b72e29a4b010922af83421356f84f26672d
jacobsnider <jakesniderlmt@gmail.com>
2021-01-21 15:36:59 -0500

    setTimeout

---
+++ Callback Functions/src/surprise.js
@@ -0,0 +1,9 @@
+const surpriseSection = document.getElementById('surprise');
+
+// function showSurprise() {
+//     () => surpriseSection.textContent = '🎉 Surprise! 🎉';
+// }
+
+const randomTime = Math.random() * 4000;
+
+setTimeout(() => surpriseSection.textContent = '🎉 Surprise! 🎉', randomTime);
